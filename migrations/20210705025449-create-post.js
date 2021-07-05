70a549ef2ff11a0de127e6946e61735f3dce334f
gallant-dev <adamgallant6@gmail.com>
2021-07-04 22:57:10 -0400

    Added basic models for Comment, Post, and  Section.

---
+++ migrations/20210705025449-create-post.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      section: {
+        type: Sequelize.STRING
+      },
+      user: {
+        type: Sequelize.STRING
+      },
+      body: {
+        type: Sequelize.STRING
+      },
+      visits: {
+        type: Sequelize.INTEGER
+      },
+      crap: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Posts');
+  }
+};
